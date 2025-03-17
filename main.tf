2a241e0785428c584385058e4deb0b4292d8c24f
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-17 18:12:38 +0530

    changes

--- main.tf
+++ main.tf
@@ -11,6 +11,21 @@ resource "aws_lb" "main" {
 resource "aws_security_group" "main" {
   name        = var.internal ? "private-alb-sg" : "public-alb-sg"
   vpc_id      = var.vpc_id
-
   tags = merge(local.tags, { Name = "${var.env}-alb-sg" })
+
+  ingress {
+    description = "APP"
+    from_port   = var.sg_port
+    to_port     = var.sg_port
+    protocol    = "tcp"
+    cidr_blocks = var.ssh_ingress
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
