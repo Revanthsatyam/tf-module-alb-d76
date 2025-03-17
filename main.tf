aa4e6b9beee1d83ae3c22cd9f5c5c832d5c0ca4a
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-17 18:07:21 +0530

    changes

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_lb" "main" {
 }
 
 resource "aws_security_group" "main" {
-  name        = "${var.env}-alb-sg"
+  name        = var.internal ? "private-alb-sg" : "public-alb-sg"
   vpc_id      = var.vpc_id
 
   tags = merge(local.tags, { Name = "${var.env}-alb-sg" })
